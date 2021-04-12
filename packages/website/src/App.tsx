7dd5f3ceb06fc2dd894409957b86817332bb4c99
Fraser McCallum <fraser@themccallums.net.nz>
2021-04-13 02:14:07 +1200

    🎉 Setup routing

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Page from "./core/Page";
+import "./styles/global.css";
+import "./styles/tailwind.output.css";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Theme from "./core/Theme";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import ServerSummary from "./pages/Dashboard/ServerSummary";
+import { ApolloProvider } from "@apollo/client";
+import GraphClient from "./graphql/apollo";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Theme>
+      <ApolloProvider client={GraphClient}>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={() => <Home />} />
+            <Route path="/dashboard" exact render={() => <Dashboard />} />
+            <Route
+              path="/dashboard/summary"
+              exact
+              render={() => <Dashboard location="summary" />}
+            />
+            <Route
+              path="/dashboard/modules"
+              exact
+              render={() => <Dashboard location="modules" />}
+            />
+            <Route
+              path="/dashboard/logs"
+              exact
+              render={() => <Dashboard location="logs" />}
+            />
+          </Switch>
+        </Router>
+      </ApolloProvider>
+    </Theme>
   );
-}
+};
 
 export default App;
